11ba3cc22a0cab708f55436a9b147c278b4621ac
Jonathan Bower <bowerpower85@gmail.com>
2020-11-07 14:12:33 -0800

    Added more boilerplate to server.js and corrected morgan middleware.

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
 const express = require ('express');
-const morgan = require ('morgan');
+const logger = require ('morgan');
 const mongoose = require ('mongoose');
+const app = express();
+const path = require('path')
+
 
 const PORT = process.env.PORT || 5000
+
+app.use(logger('dev'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+
